8d4b9de53318460114953b6bd2ebf2bd24e6fa5c
FireDemon450 <117426318+FireDemon450@users.noreply.github.com>
2026-03-13 16:19:09 +0000

    edited code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld { 
     public static void main (String [] args) { 
-        String name = "John";
+        String name = "Eric";
         System.out.println (name); 
     } 
 } 
