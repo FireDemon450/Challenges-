ff6ef13d1e08f8f6a5bb28885f0eceba9db3ae2d
FireDemon450 <117426318+FireDemon450@users.noreply.github.com>
2026-03-17 16:11:56 +0000

    Made code

---
+++ The_year.java
@@ -0,0 +1,15 @@
+public class The_year { 
+    public static void main(String[] args){ 
+        int year = 2067; 
+        System.out.println("Welcome to Java!"); 
+        System.out.println("PJ: Hey Eric, what's the year?"); 
+         System.out.println("Eric: The year is. " + year);
+        System.out.println("PJ: Wait...Say that again?"); 
+        System.out.println("   PJ: "); 
+        System.out.println("  (',') "); 
+        System.out.println("_6_/|\\_7_"); 
+        System.out.println("    |    "); 
+        System.out.println("    /\\  "); 
+        System.out.println("   /  \\       "); 
+    }
+} 
