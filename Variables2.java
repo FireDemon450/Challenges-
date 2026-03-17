c58627cf29d345bb6122e0a1b3eaca3b1305efbe
FireDemon450 <117426318+FireDemon450@users.noreply.github.com>
2026-03-17 16:33:14 +0000

    code

--- Variables2.java
+++ Variables2.java
@@ -1,15 +1,14 @@
 public class Variables2 {
-    public static void main (String[] args){ 
+    public static void main (String [] args){ 
         int age = 15; 
         double price = 4.99; 
         boolean isStudent = true; 
         char grade = 'A'; 
 
-        System.out.println("Age: " = age); 
         System.out.println("Age: " + age); 
         System.out.println("Price: " + price); 
         System.out.println("Student: " + isStudent); 
-        System.out.println("Grade: " +grade); 
+        System.out.println("Grade: " + grade); 
 
     } 
 }
